a303bfd73d052c1a7e1f9ac72deffda80a1776c9
ValleyRescueTraining <valleyrescuetraining@icloud.com>
2026-04-14 20:11:00 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,4 @@
-const firebaseConfig = {
+window.VRT_FIREBASE_CONFIG = {
   apiKey: "AIzaSyAriwyxYV65QJmYR-vjlzFCtfDhkSzONV8",
   authDomain: "vrt-acls-sim.firebaseapp.com",
   databaseURL: "https://vrt-acls-sim-default-rtdb.firebaseio.com",
@@ -7,5 +7,3 @@ const firebaseConfig = {
   messagingSenderId: "13977363558",
   appId: "1:13977363558:web:fd40981c97688410aa39a7"
 };
-
-window.firebaseConfig = firebaseConfig;
